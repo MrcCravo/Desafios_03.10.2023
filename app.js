391b09081b6d59de8dc9a60d8ea15a20353e1102
MrcCravo <ferramentas@maracs.com.br>
2023-10-03 21:01:58 -0300

    Exercício realizado

---
+++ app.js
@@ -0,0 +1,9 @@
+let diaDaSemana = 0;
+diaDaSemana = prompt ("Que dia é hoje?");
+
+if (diaDaSemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("Bom fim de semana!");
+} else {
+console.log("Boa Semana!");
+}
+
